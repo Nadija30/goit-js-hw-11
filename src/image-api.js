9b5a05a23e4757c82cf6563407c3c5c4bd69b2a3
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-08-03 13:07:02 +0300

    commit

--- src/image-api.js
+++ src/image-api.js
@@ -1,10 +1,19 @@
 import axios from "axios";
-axios.defaults.baseURL ='https://pixabay.com/api/';
-axios.defaults.headers.common["x-api-key"] = '38622726-1d1c6ccd64174273156cb8786';
+const URL ='https://pixabay.com/api/';
+const KEY = '38622726-1d1c6ccd64174273156cb8786';
 
 
-export async function fetchPhoto(q, page, perPage) {
-    const url = `${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
+// axios.defaults.baseURL ='https://pixabay.com/api/';
+// axios.defaults.headers.common["x-api-key"] = '38622726-1d1c6ccd64174273156cb8786';
+const options = {
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch:  "true",
+}
+
+export async function searchPhoto(q, page, perPage) {
+    const url = axios.get (`${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`);
     const response = await axios.get(url);
     return response.data;          
 };
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './styles.css';
+import { searchPhoto } from './image-api'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.search-form');
 const galleryImage = document.querySelector('.gallery');
 const button = document.querySelector('.load-more');
+
+
+const perPage = 40;
+let page = 1;
+let valueSearchPhoto = '';
