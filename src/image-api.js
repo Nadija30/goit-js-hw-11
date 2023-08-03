b386c88b2b75f511d608af704f16f3e659ef43c7
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-08-03 13:23:34 +0300

    commit

--- src/image-api.js
+++ src/image-api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-const URL ='https://pixabay.com/api/';
-const KEY = '38622726-1d1c6ccd64174273156cb8786';
+// const URL ='https://pixabay.com/api/';
+// const KEY = '38622726-1d1c6ccd64174273156cb8786';
 
 
-// axios.defaults.baseURL ='https://pixabay.com/api/';
-// axios.defaults.headers.common["x-api-key"] = '38622726-1d1c6ccd64174273156cb8786';
+axios.defaults.baseURL ='https://pixabay.com/api/';
+axios.defaults.headers.common["x-api-key"] = '38622726-1d1c6ccd64174273156cb8786';
 const options = {
     image_type: "photo",
     orientation: "horizontal",
@@ -12,7 +12,7 @@ const options = {
 }
 
 export async function searchPhoto(q, page, perPage) {
-    const url = axios.get (`${URL}?key=${KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`);
+    const url = axios.get (`&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`);
     const response = await axios.get(url);
     return response.data;          
 };
